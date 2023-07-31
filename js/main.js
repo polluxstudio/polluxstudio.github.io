b6b0b7630ed0601ce9b0a8eaddc9116b3f3faac7
Tafhimul kabir <tafhimulkabir@protonmail.com>
2023-08-01 00:08:28 +0600

    Changed carousel with owl carousel

--- js/main.js
+++ js/main.js
@@ -25,101 +25,153 @@ showSlides();
 
 
 // Team Slider
-function setupSlider(sectionId) {
-  const section = document.getElementById(sectionId);
-  const wrapper = section.querySelector('.app__carousel__wrapper');
-  const carouselContainer = wrapper.querySelector('.app__carousel__container');
-  const firstCardWidth = carouselContainer.querySelector('.app__single__carousel').offsetWidth;
-  const arrowBtns = wrapper.querySelectorAll('.app__carousel__arrow');
-  const sliderItems = [...carouselContainer.children];
+$('#app__team__carousel').owlCarousel({
+  loop:true,
+  margin:30,
+  nav:true,
+  autoplay:true,
+  autoplayTimeout:1000,
+  autoplayHoverPause:true,
+  responsive:{
+      0:{
+          items:1
+      },
+      600:{
+          items:2
+      },
+      1000:{
+          items:3
+      }
+  }
+});
 
-  let isDragging = false;
-  let startX, startScrollLeft, timeoutId;
+// Testimonial Slider
+$('#app__testimonial__carousel').owlCarousel({
+  loop:true,
+  margin:30,
+  nav:true,
+  autoplay:true,
+  autoplayTimeout:1000,
+  autoplayHoverPause:true,
+  responsive:{
+      0:{
+          items:1
+      },
+      600:{
+          items:1
+      },
+      1000:{
+          items:2
+      }
+  }
+});
 
-  // Get the number of items that can fit in the carousel at once
-  const itemsPerView = Math.round(carouselContainer.offsetWidth / firstCardWidth);
 
-  // Insert copies of the last few items to the beginning of the carousel for infinite scrolling
-  sliderItems.slice(-itemsPerView).reverse().forEach(item => {
-    carouselContainer.insertAdjacentHTML('afterbegin', item.outerHTML);
-  });
 
-  // Insert copies of the first few items to the end of the carousel for infinite scrolling
-  sliderItems.slice(0, itemsPerView).forEach(item => {
-    carouselContainer.insertAdjacentHTML('beforeend', item.outerHTML);
-  });
 
-  // Scroll the carousel to an appropriate position to hide first few duplicate items on Firefox
-  carouselContainer.classList.add('no-transition');
-  carouselContainer.scrollLeft = carouselContainer.offsetWidth;
-  carouselContainer.classList.remove('no-transition');
+// function setupSlider(sectionId) {
+//   const section = document.getElementById(sectionId);
+//   const wrapper = section.querySelector('.app__carousel__wrapper');
+//   const carouselContainer = wrapper.querySelector('.app__carousel__container');
+//   const firstCardWidth = carouselContainer.querySelector('.app__single__carousel').offsetWidth;
+//   const arrowBtns = wrapper.querySelectorAll('.app__carousel__arrow');
+//   const sliderItems = [...carouselContainer.children];
 
-  // Add event listeners for the arrow buttons to scroll the carousel left and right
-  arrowBtns.forEach(btn => {
-    btn.addEventListener('click', () => {
-      carouselContainer.scrollLeft += btn.classList.contains('slider-arrow-left') ? -firstCardWidth : firstCardWidth;
-    });
-  });
+//   let isDragging = false;
+//   let startX, startScrollLeft, timeoutId;
 
-  const dragStart = (e) => {
-    isDragging = true;
-    carouselContainer.classList.add('dragging');
-    // Records the initial cursor and scroll position of the carousel
-    startX = e.pageX;
-    startScrollLeft = carouselContainer.scrollLeft;
-  };
+//   // Get the number of items that can fit in the carousel at once
+//   const itemsPerView = Math.round(carouselContainer.offsetWidth / firstCardWidth);
 
-  const dragging = (e) => {
-    if (!isDragging) return; // if isDragging is false return from here
-    // Updates the scroll position of the carousel based on the cursor movement
-    carouselContainer.scrollLeft = startScrollLeft - (e.pageX - startX);
-  };
+//   // Insert copies of the last few items to the beginning of the carousel for infinite scrolling
+//   sliderItems.slice(-itemsPerView).reverse().forEach(item => {
+//     carouselContainer.insertAdjacentHTML('afterbegin', item.outerHTML);
+//   });
 
-  const dragStop = () => {
-    isDragging = false;
-    carouselContainer.classList.remove('dragging');
-  };
+//   // Insert copies of the first few items to the end of the carousel for infinite scrolling
+//   sliderItems.slice(0, itemsPerView).forEach(item => {
+//     carouselContainer.insertAdjacentHTML('beforeend', item.outerHTML);
+//   });
 
-  const infiniteScroll = () => {
-    // If the carousel is at the beginning, scroll to the end
-    if (carouselContainer.scrollLeft === 0) {
-      carouselContainer.classList.add('no-transition');
-      carouselContainer.scrollLeft = carouselContainer.scrollWidth - (2 * carouselContainer.offsetWidth);
-      carouselContainer.classList.remove('no-transition');
-    }
-    // If the carousel is at the end, scroll to the beginning
-    else if (Math.ceil(carouselContainer.scrollLeft) === carouselContainer.scrollWidth - carouselContainer.offsetWidth) {
-      carouselContainer.classList.add('no-transition');
-      carouselContainer.scrollLeft = carouselContainer.offsetWidth;
-      carouselContainer.classList.remove('no-transition');
-    }
+//   // Scroll the carousel to an appropriate position to hide first few duplicate items on Firefox
+//   carouselContainer.classList.add('no-transition');
+//   carouselContainer.scrollLeft = carouselContainer.offsetWidth;
+//   carouselContainer.classList.remove('no-transition');
 
-    // Clear existing timeout & start autoplay if the mouse is not hovering over the carousel
-    clearTimeout(timeoutId);
-    if (!wrapper.matches(':hover')) {
-      autoPlay();
-    }
-  };
+//   // Add event listeners for the arrow buttons to scroll the carousel left and right
+//   arrowBtns.forEach(btn => {
+//     btn.addEventListener('click', () => {
+//       carouselContainer.scrollLeft += btn.classList.contains('slider-arrow-left') ? -firstCardWidth : firstCardWidth;
+//     });
+//   });
 
-  const autoPlay = () => {
-    if (window.innerWidth < 800) return; // Return if window is smaller than 800
-    // Autoplay the carousel after every 2500 ms
-    timeoutId = setTimeout(() => carouselContainer.scrollLeft += firstCardWidth, 2500);
-  };
-  autoPlay();
+//   const dragStart = (e) => {
+//     isDragging = true;
+//     carouselContainer.classList.add('dragging');
+//     // Records the initial cursor and scroll position of the carousel
+//     startX = e.pageX;
+//     startScrollLeft = carouselContainer.scrollLeft;
+//   };
 
-  carouselContainer.addEventListener('mousedown', dragStart);
-  carouselContainer.addEventListener('mousemove', dragging);
-  document.addEventListener('mouseup', dragStop);
-  carouselContainer.addEventListener('scroll', infiniteScroll);
-  wrapper.addEventListener('mouseenter', () => clearTimeout(timeoutId));
-  wrapper.addEventListener('mouseleave', autoPlay);
-}
+//   const dragging = (e) => {
+//     if (!isDragging) return; // if isDragging is false return from here
+//     // Updates the scroll position of the carousel based on the cursor movement
+//     carouselContainer.scrollLeft = startScrollLeft - (e.pageX - startX);
+//   };
 
-// Call the setupSlider function for each section with its unique ID
-setupSlider('app__team');
-setupSlider('app__gallery');
-setupSlider('app__testimonial');
+//   const dragStop = () => {
+//     isDragging = false;
+//     carouselContainer.classList.remove('dragging');
+//   };
+
+//   const infiniteScroll = () => {
+//     // If the carousel is at the beginning, scroll to the end
+//     if (carouselContainer.scrollLeft === 0) {
+//       carouselContainer.classList.add('no-transition');
+//       carouselContainer.scrollLeft = carouselContainer.scrollWidth - (2 * carouselContainer.offsetWidth);
+//       carouselContainer.classList.remove('no-transition');
+//     }
+//     // If the carousel is at the end, scroll to the beginning
+//     else if (Math.ceil(carouselContainer.scrollLeft) === carouselContainer.scrollWidth - carouselContainer.offsetWidth) {
+//       carouselContainer.classList.add('no-transition');
+//       carouselContainer.scrollLeft = carouselContainer.offsetWidth;
+//       carouselContainer.classList.remove('no-transition');
+//     }
+
+//     // Clear existing timeout & start autoplay if the mouse is not hovering over the carousel
+//     clearTimeout(timeoutId);
+//     if (!wrapper.matches(':hover')) {
+//       autoPlay();
+//     }
+//   };
+
+//   const autoPlay = () => {
+//     if (window.innerWidth < 800) return; // Return if window is smaller than 800
+//     // Autoplay the carousel after every 2500 ms
+//     timeoutId = setTimeout(() => carouselContainer.scrollLeft += firstCardWidth, 2500);
+//   };
+//   autoPlay();
+
+//   carouselContainer.addEventListener('mousedown', dragStart);
+//   carouselContainer.addEventListener('mousemove', dragging);
+//   document.addEventListener('mouseup', dragStop);
+//   carouselContainer.addEventListener('scroll', infiniteScroll);
+//   wrapper.addEventListener('mouseenter', () => clearTimeout(timeoutId));
+//   wrapper.addEventListener('mouseleave', autoPlay);
+// }
+
+// // Call the setupSlider function for each section with its unique ID
+// setupSlider('app__team');
+// setupSlider('app__gallery');
+// setupSlider('app__testimonial');
+
+
+
+
+
+
+
+
 // const teamWrapper = document.querySelector('.app__carousel__wrapper');
 // const carouselContainer = document.querySelector('.app__carousel__container');
 // const firstCardWidth = carouselContainer.querySelector('.app__single__carousel').offsetWidth;
@@ -208,6 +260,26 @@ setupSlider('app__testimonial');
 // teamWrapper.addEventListener('mouseenter', () => clearTimeout(timeoutId));
 // teamWrapper.addEventListener('mouseleave', autoPlay);
 
+// Gallery Carousel
+$('#app__gallery__carousel').owlCarousel({
+  loop:true,
+  margin:10,
+  nav:true,
+  autoplay:true,
+  autoplayTimeout:1000,
+  autoplayHoverPause:true,
+  responsive:{
+      0:{
+          items:1
+      },
+      600:{
+          items:2
+      },
+      1000:{
+          items:3
+      }
+  }
+});
 
 // FAQ Toggle Options
 const faqHeaders = document.querySelectorAll('#app__faq .app__single__faq .app__single__faq__header');
